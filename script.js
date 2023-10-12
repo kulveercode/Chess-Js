380d3be8427d261c6fc9762dd8ae8735fc138abd
Kulveer Singh <kulveer.coder@gmail.com>
2023-10-12 23:00:48 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// Inserting the Images
+function insertImage(){
+
+    document.querySelectorAll(".box").forEach(image => {
+
+        if (image.innerText.length !== 0) {
+            if (image.innerText == 'Wpawn' || image.innerText == 'Bpawn') {
+                image.innerHTML = `${image.innerText} <img class='allimg allpawm' src='${image.innerText}.png' alt=''>`
+                image.style.cursor = 'pointer'
+            }
+            else {
+                image.innerHTML = ` ${image.innerText} <img class='allimg' src='${image.innerText}.png' alt=''>`
+                image.style.cursor = 'pointer'
+            }
+        }
+    })
+}
+insertImage()
+
+//Coloring
+
+function coloring() {
+    const color = document.querySelectorAll('.box')
+
+    color.forEach(color => {
+        getId = color.id
+        arr = Array.from(getId)
+        arr.shift()
+        aside = aval(arr.pop())
+        aup = eval(arr.shift())
+        a = aside + aup
+
+        if(a % 2 == 0){
+            color.style.backgroundColor = 'rgb(240, 201, 150)'
+        }
+        if(a % 2 !== 0){
+            color.style.backgroundColor = 'rgb(100, 75, 43)'
+        }
+    })
+}
+coloring()
+
